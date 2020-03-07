375a9adad459e006b9caba623cbbdfe151d4c676
Gasa91 <yoanysanchez1212@gmail.com>
2020-03-06 21:16:02 -0500

    funciones

---
+++ funciones.php
@@ -0,0 +1,21 @@
+<?php
+
+//funcion para calcular la edad de una edad
+
+function calcularEdad($nacimiento){
+    $edad=2020-$nacimiento;
+    echo("La edad de la persona es: $edad");
+
+}
+
+function calcularEdad2($nacimiento, $actual){
+    $edad=$actual-$nacimiento;
+    echo("La edad de la personas es: $edad");
+
+
+}
+function calcularEdad3($nacimiento, $actual){
+    $edad=$actual-$nacimiento;
+    return ($edad); 
+}
+?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+include('funciones.php');
 
 echo("clase 4 bucles, arreglos y funciones ");
 //  arreglo en php 
@@ -65,4 +65,46 @@ $usuarios=array(
     'usuario4'=>array('nombre'=>"Alenjandro",'edad'=>34,'genero'=>"Masculino"),
     'usuario5'=>array('nombre'=>"María",'edad'=>23,'genero'=>"Femenino")
 );
+echo("<br>");
+print_r($usuarios);
+
+//ciclo foreach para acceder alos elementos del arreglo multidimencionales
+echo("<br>....................................<br>");
+foreach($usuarios as $clavesArreglosUnidimensionales=>$arreglosUnidimensionales){
+    echo($clavesArreglosUnidimensionales);
+    echo("<br>");
+    print_r($arreglosUnidimensionales);
+    echo("<br>");
+    foreach($arreglosUnidimensionales as $claves=>$valorers){
+        echo("<br>");
+       echo($claves) ;
+       echo("<br>");
+       echo($valorers);
+       echo("<br>");
+
+    }
+}
+
+//calcular la edad de dos personas 
+echo("<br>....................................<br>");
+echo("<br>");
+calcularEdad(1991);
+
+echo("<br>....................................<br>");
+echo("<br>");
+calcularEdad2(1991,2020);
+
+echo("<br>....................................<br>");
+echo("<br>");
+$edad3=calcularEdad3(1991,2020);
+echo("La edad de la personas es: $edad3");
+
+
+//arreglo que se llena con una funcion 
+echo("<br>....................................<br>");
+echo("<br>");
+$edades=array('edad1'=>calcularEdad3(1991,2020),'edad'=>calcularEdad3(1995,2020));
+    print_r($edades);
+
+
 ?>
